15b2ecee6199e2aac993000f42a2189ad96ffcfd
BeloglazovaEV <beloglazova.e.v.00@gmail.com>
2025-10-07 23:24:14 +0500

    Update  last name app1.py

--- app1.py
+++ app1.py
@@ -3,7 +3,7 @@ import streamlit1
 import app
 st.title("Многостраничное Streamlit-приложение")
 page = st.sidebar.selectbox("Выберите страницу", ["Страница 1", "Страница 2"])
-if page == "Страница 1":
+if page == "Белоглазова":
     streamlit1.show_page()
-elif page == "Страница 2":
+elif page == "Усачёва":
     app.show_page()
