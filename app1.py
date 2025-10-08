4006866cfd9b914173b451ff100e8aeacfa7df2f
BeloglazovaEV <beloglazova.e.v.00@gmail.com>
2025-10-08 20:33:47 +0500

    Добавила нейронку

--- app1.py
+++ app1.py
@@ -2,8 +2,10 @@ import streamlit as st
 import streamlit1
 import app
 st.title("Многостраничное Streamlit-приложение")
-page = st.sidebar.selectbox("Выберите страницу", ["Белоглазова", "Усачёва"])
+page = st.sidebar.selectbox("Выберите страницу", ["Белоглазова", "Усачёва", "Белоглазова, Нейронка"])
 if page == "Белоглазова":
     streamlit1.show_page()
 elif page == "Усачёва":
     app.show_page()
+elif page == "Белоглазова, нейронка":
+    neiroKate.show_page()
